6d3ea03ec01369867b5931f1f2a0fca54dfb1d08
Jacob Klapwijk <jacob.klapwijk@mentimeter.com>
2019-12-02 09:59:45 +0100

    day 1: puzzle 2

--- 1/index.js
+++ 1/index.js
@@ -1,11 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-const input = fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8");
-
-const result = input
+const input = fs
+  .readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
   .split("\n")
-  .map(a => parseInt(a))
-  .reduce((acc, curr) => acc + Math.floor(curr / 3) - 2, 0);
+  .map(a => parseInt(a));
 
-console.log(result);
+const calculateFuel = mass => Math.floor(mass / 3) - 2;
+
+const result1 = input.reduce((acc, curr) => acc + calculateFuel(curr), 0);
+const result2 = input.reduce((acc, curr) => {
+  while (curr > 0) {
+    curr = calculateFuel(curr);
+    if (curr > 0) {
+      acc = acc + curr;
+    }
+  }
+  return acc;
+}, 0);
+
+console.log({ result1, result2 });
