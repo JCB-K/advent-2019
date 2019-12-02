66b24d7c77d69208c8d3d5d85f2b5c2b1c30fef1
Jacob Klapwijk <jacob.klapwijk@mentimeter.com>
2019-12-02 14:16:42 +0100

    day 2: puzzle 2

--- 2/index.js
+++ 2/index.js
@@ -1,30 +1,50 @@
 const fs = require("fs");
 const path = require("path");
 
-const input = fs
-  .readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
-  .split(",")
-  .map(a => parseInt(a));
+const getInput = () =>
+  fs
+    .readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
+    .split(",")
+    .map(a => parseInt(a));
 
-input[1] = 12;
-input[2] = 2;
+const calc = input => {
+  let index = 0;
 
-let index = 0;
+  while (index < input.length) {
+    const opCode = input[index];
 
-while (index < input.length) {
-  const opCode = input[index];
+    if (opCode === 99) {
+      break;
+    }
 
-  if (opCode === 99) {
-    break;
+    const input1 = input[input[index + 1]];
+    const input2 = input[input[index + 2]];
+
+    const newVal = opCode === 1 ? input1 + input2 : input1 * input2;
+    input[input[index + 3]] = newVal;
+
+    index += 4;
   }
+  return input[0];
+};
 
-  const input1 = input[input[index + 1]];
-  const input2 = input[input[index + 2]];
+const part1 = (data, noun, verb) => {
+  data[1] = noun;
+  data[2] = verb;
+  return calc(data);
+};
 
-  const newVal = opCode === 1 ? input1 + input2 : input1 * input2;
-  input[input[index + 3]] = newVal;
+const part2 = () => {
+  for (let noun = 0; noun < 99; noun++) {
+    for (let verb = 0; verb < 99; verb++) {
+      if (part1(getInput(), noun, verb) === 19690720) {
+        return 100 * noun + verb;
+      }
+    }
+  }
+};
 
-  index += 4;
-}
-
-console.log(input[0]);
+console.log({
+  part1: part1(getInput(), 12, 2),
+  part2: part2()
+});
